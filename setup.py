e8bf8703cc90a95f3b415a7586c4c4d4f74b021c
aliva <ali.vakilzade@gmail.com>
2013-01-21 00:05:58 +0330

    setup.py created

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='djangorunstandalone',
+    version='0.0.1',
+    description='run django project as standalone application inside a window',
+    author='Ali Vakilzade',
+    author_email='ali.vakilzade@gmail.com',
+    url='https://bitbucket.org/aliva/django-runstandalone',
+    packages=[
+        'djangorunstandalone',
+    ],
+)
